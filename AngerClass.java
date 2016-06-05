b38980a8c7477ff4a6d5efe12c41b8890158157f
slugbunny <tinycc99@gmail.com>
2016-06-04 23:27:51 -0400

    Update AngerClass.java

--- AngerClass.java
+++ AngerClass.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class JoyClass() extends Application{
+public class AngerClass() extends Application{
   public static void main(String[] args){
     launch(args);
   }
