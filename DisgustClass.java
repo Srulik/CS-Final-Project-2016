7fd5cb4f7ef796b4e2faea3d352ade96c5abf8f5
slugbunny <tinycc99@gmail.com>
2016-06-08 14:13:18 -0400

    Update DisgustClass.java

--- DisgustClass.java
+++ DisgustClass.java
@@ -15,8 +15,23 @@ public class DisgustClass() extends Application{
   
   public void start() throws Exception{
     Stage window = new Stage();
+    window.setTitle("Disgust!");
+    window.setWidth(300);
+    window.setHeight(400);
+    
+    int num = 0;
+    
+    Button disgust = new Button("Wanna watch this video?");
+    disgust.setOnAction( e -> https://www.youtube.com/watch?v=XM9pmmrKXKA);
+    
+    if(num == 1){
+      Button exit = new Button("EXIT");
+      exit.setOnAction(e -> window.close());
+    }
     
     MediaPlayer play - new MediaPlayer()
     
+    window.show();
   }
+
 }
