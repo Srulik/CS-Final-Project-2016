0c67167f651a5774e4a5a61af7f1798e41aa1f5a
Srulik <Srulik@users.noreply.github.com>
2016-06-08 14:29:26 -0400

    Made it work I think, only with Anger class for now
    Uncomment and add to the buttons StackPane to add other classes, also edit other classes cause they need some fixing to be compatible

--- Game.java
+++ Game.java
@@ -1,26 +1,38 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class Game() extends Application{
+public class Game extends Application{
   public static void main(String[] args){
-    
+	  launch(args);
   }
   
   public void start(Stage p){
-    Stage window = new Stage();
+    Stage window = p;
     window.setTitle("Game");
     window.setMinWidth(300);
     window.setMinHeight(400);
     
     Label label = new Label();
-    label.setText("How are you feeling right now?")
+    label.setText("How are you feeling right now?");
+    
+    /*
     Button joy = new Button("Joy");
     joy.setOnAction(e -> {
 	JoyClass.start();		
@@ -41,14 +53,23 @@ public class Game() extends Application{
 	DisgustClass.start();		
 		});
 		
+		*/
+		
+    Stage angerStage = new Stage();
     Button anger = new Button("Anger");
     anger.setOnAction(e -> {
-	AngerClass.start();		
+	AngerClass.startAnger();		
 		});
 		
-	VBox buttons = new VBox(50);
-	buttons.getChildren().addAll(joy, sad, fear, disgust, anger);
-	Scene scene = new Scene(buttons, 600, 600);
+	StackPane buttons = new StackPane();
+	buttons.getChildren().addAll(label,anger); //add joy, sad, fear, disgust,
+	BorderPane layout = new BorderPane();;
+	BorderPane.setAlignment(buttons, Pos.CENTER);
+	layout.setCenter(buttons);
+	StackPane full = new StackPane();
+	full.setAlignment(anger, Pos.CENTER);;
+	full.getChildren().addAll(layout);
+	Scene scene = new Scene(full);
 	window.setScene(scene);
 	window.show();
   }
