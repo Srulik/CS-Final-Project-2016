cb987988fe6db5cf3807438944b6cee8e5e97dad
Srulik <Srulik@users.noreply.github.com>
2016-06-14 10:18:02 -0400

    Update JoyClass.java

--- JoyClass.java
+++ JoyClass.java
@@ -1,35 +1,46 @@
+package net.mrpaul.ads.TM250.project;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-public class JoyClass() extends Application{
-  public static void main(String[] args){
-    launch(args);
-  }
+public class JoyClass extends Game{
 
-  public void start(){
-    Stage window = new Stage();
-    window.setTitle("Joy!");
-    window.setWidth(300);
-    window.setHeight(400);
-    
-    Label message = new Label("You are now happy ! Have a nice day :)");
-    Button exit = new Button("EXIT");
-    exit.setOnAction(e -> window.close());
-    
-    VBox a = new VBox(50);
-    a.getChildren.addAll(message, exit);
-    
-    Scene scene = new Scene(a, 600,600);
-    window.setScene(scene);
-    window.show();
-  }
+	  public static void startJoy(){
+	    Stage window = new Stage();
+	    window.setTitle("Joy!");
+	    window.setWidth(300);
+	    window.setHeight(400);
+	    
+	    Label message = new Label("You are happy now! Have a nice day :)");
+	    Label played = new Label("You played " + plays + " games!");
+	    
+	    Button exit = new Button("EXIT");
+	    exit.setOnAction(e -> window.close());
+	    
+	    VBox a = new VBox(50);
+	    a.getChildren().addAll(played, message, exit);
+	    
+	    a.setAlignment(Pos.CENTER);
+	    Scene scene = new Scene(a, 600,600);
+	    window.setScene(scene);
+	    window.show();
+	  }
 
-}
-
-
+	}
